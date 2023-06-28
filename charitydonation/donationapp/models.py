748dfe5ac4412231438e610fd25c710cae674778
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-06-28 08:54:36 +0200

    Added models Category and Institution

--- charitydonation/donationapp/models.py
+++ charitydonation/donationapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+TYPES = [
+    ("Fundacja", "FA"),
+    ("Organizacja pozarządowa", "OP"),
+    ("Zbiórka lokalna", "ZL")
+]
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.CharField(choices=TYPES)
+    categories = models.ManyToManyField(Category)
