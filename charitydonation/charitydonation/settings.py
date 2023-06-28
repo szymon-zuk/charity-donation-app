379497e3b33bb82d33856803a9b6ddaff947c570
Szymon Żuk <116501563+szymon-zuk@users.noreply.github.com>
2023-06-28 12:48:49 +0200

    Added Donation model and modified choices type in Institution model

--- charitydonation/charitydonation/settings.py
+++ charitydonation/charitydonation/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "donationapp"
+    "donationapp",
+    "phone_field"
 ]
 
 MIDDLEWARE = [
