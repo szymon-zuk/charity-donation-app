b95b5f6250c158f33d08d91bf6549b13dd19bbe7
szymon-zuk <szymon.p.zuk@gmail.com>
2023-07-08 13:14:08 +0200

    Added login functionality

--- charitydonation/donationapp/views.py
+++ charitydonation/donationapp/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.generic import CreateView
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .models import Donation, Institution
 
 
@@ -32,7 +33,21 @@ class AddDonation(View):
 
 class Login(View):
     def get(self, request):
-        return render(request, "login.html")
+        form = LoginForm()
+        return render(request, "login.html", {"form": form})
+
+    def post(self, request):
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data["email"]
+            password = form.cleaned_data["password"]
+            user = authenticate(request, username=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("landing-page")
+            else:
+                return redirect("register")
+        return render(request, "login.html", {"form": form})
 
 
 class Register(View):
